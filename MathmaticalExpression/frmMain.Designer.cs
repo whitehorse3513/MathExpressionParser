63cf68f1888b911a8d7fbd8d7d95f025dd8da595
whitehorse3513 <white.horse3513@gmail.com>
2019-09-15 22:46:56 +0800

    Parser

---
+++ MathmaticalExpression/frmMain.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MathmaticalExpression
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtExpression = new System.Windows.Forms.TextBox();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Expression:";
+            // 
+            // txtExpression
+            // 
+            this.txtExpression.Location = new System.Drawing.Point(111, 28);
+            this.txtExpression.Name = "txtExpression";
+            this.txtExpression.Size = new System.Drawing.Size(285, 20);
+            this.txtExpression.TabIndex = 1;
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(321, 68);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(75, 23);
+            this.btnParse.TabIndex = 2;
+            this.btnParse.Text = "Parse";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Result:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(111, 111);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(285, 20);
+            this.txtResult.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 164);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtExpression);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Mathmatical Expression";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtExpression;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ MathmaticalExpression/frmMain.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathmaticalExpression
+{
+    public partial class frmMain : Form
+    {
+        public string ExprString;
+        public int LexPos;
+        public int LexicalAttribute;
+        public string LexicalAttribute2;
+        public string PostFixString;
+        public int Given;
+
+        public const int NONE = 0;
+        public const int LargestSize = 54;
+
+        public string LookAhead;
+
+        static Dictionary<string, int> id_dict = new Dictionary<string, int>();
+        static int id_count = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetVariables()
+        {
+            Given = 0;
+            LexPos = 0;
+            ExprString = txtExpression.Text;
+            PostFixString = "";
+        }
+
+        private string LexicalAnalyzer()
+        {
+            string Temp = "";
+            while (LexPos < ExprString.Length)
+            {
+                LexicalAttribute = 0;
+                LexicalAttribute2 = "";
+                LexPos++;
+
+                switch (ExprString[LexPos - 1])
+                {
+                    case ' ':
+                    case (char)9:
+                        break;
+                    case '\0':
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '\\':
+                    case ':':
+                    case '(':
+                    case ')':
+                    case ',':
+                        return ExprString[LexPos - 1].ToString();
+                    case (char)13:
+                        if (ExprString[LexPos] != 10)
+                            SyntaxError();
+                        else
+                            LexPos++;
+                        break;
+                    case (char)10:
+                        SyntaxError();
+                        break;
+                    default:
+                        if (IsAlpha(ExprString[LexPos - 1].ToString()))
+                        {
+
+                            while (IsAlphaNumberic(ExprString[LexPos - 1].ToString()))
+                            {
+                                Temp = Temp + ExprString[LexPos - 1].ToString();
+                                LexPos++;
+                                if (LexPos > ExprString.Length)
+                                    break;
+                            }
+                            LexPos--;
+                            if (Temp.Length > LargestSize)
+                                SyntaxError();
+                            if (Temp.ToUpper() == "MOD")
+                            {
+                                return "MOD";
+                            }
+                            else if (LookUpFunctionNumber(Temp) != 0)
+                            {
+                                LexicalAttribute = LookUpFunctionNumber(Temp);
+                                LexicalAttribute2 = Temp.ToUpper();
+                                return "FUNC";
+                            }
+                            else
+                            {
+                                LexicalAttribute = GetID(Temp);
+                                return "ID";
+                            }
+                        }
+                        else if (IsNumber(ExprString[LexPos - 1].ToString()))
+                        {
+                            while (IsNumber(ExprString[LexPos - 1].ToString()))
+                            {
+                                Temp = Temp + ExprString[LexPos - 1].ToString();
+                                LexPos++;
+                                if (LexPos > ExprString.Length)
+                                    break;
+                            }
+                            LexicalAttribute = Convert.ToInt32(Temp);
+                            LexPos = LexPos - 1;
+                            return "NUM";
+                        }
+                        break;
+                }
+            }
+            return "";
+        }
+
+        private int GetID(string IDName)
+        {
+            if (id_dict.ContainsKey(IDName))
+            {
+                return id_dict[IDName];
+            }
+            else
+            {
+                id_dict.Add(IDName, id_count++);
+                return id_dict[IDName];
+            }
+        }
+
+        private int LookUpFunctionNumber(string FunctionName)
+        {
+            int FunctionNumber = 0;
+            switch(FunctionName)
+            {
+                case "SQR":
+                    FunctionNumber = 1;
+                    break;
+                case "SQUARE":
+                    FunctionNumber = 2;
+                    break;
+                case "CUBE":
+                    FunctionNumber = 3;
+                    break;
+                case "INT":
+                    FunctionNumber = 4;
+                    break;
+                case "FIX":
+                    FunctionNumber = 5;
+                    break;
+                case "ROUND":
+                    FunctionNumber = 6;
+                    break;
+                case "RND":
+                    FunctionNumber = 7;
+                    break;
+                case "ABS":
+                    FunctionNumber = 8;
+                    break;
+                case "SGN":
+                    FunctionNumber = 9;
+                    break;
+                case "REPROC":
+                    FunctionNumber = 10;
+                    break;
+                case "INVSGN":
+                    FunctionNumber = 11;
+                    break;
+                case "FACT":
+                    FunctionNumber = 12;
+                    break;
+                case "SIN":
+                    FunctionNumber = 13;
+                    break;
+                case "COS":
+                    FunctionNumber = 14;
+                    break;
+                case "TAN":
+                    FunctionNumber = 15;
+                    break;
+                case "SEC":
+                    FunctionNumber = 16;
+                    break;
+                case "COSEC":
+                    FunctionNumber = 17;
+                    break;
+                case "COT":
+                    FunctionNumber = 18;
+                    break;
+                case "HSIN":
+                    FunctionNumber = 19;
+                    break;
+                case "HCOS":
+                    FunctionNumber = 20;
+                    break;
+                case "HTAN":
+                    FunctionNumber = 21;
+                    break;
+                case "HSEC":
+                    FunctionNumber = 22;
+                    break;
+                case "HCOSEC":
+                    FunctionNumber = 23;
+                    break;
+                case "HCOT":
+                    FunctionNumber = 24;
+                    break;
+            }
+            return FunctionNumber;
+        }
+
+        private void MatchAndIncrement(string CheckValue)
+        {
+            if (LookAhead == CheckValue)
+                LookAhead = LexicalAnalyzer();
+            else
+                SyntaxError();
+        }
+
+        private void ParseAll()
+        {
+            LookAhead = LexicalAnalyzer();
+
+            while(LookAhead != "")
+            {
+                ParseExpression();
+            }
+        }
+
+        private void ParseExpression()
+        {
+            string Temp;
+            ParseTerm();
+
+            while(true)
+            {
+                switch(LookAhead)
+                {
+                    case "+":
+                    case "-":
+                        Temp = LookAhead;
+                        MatchAndIncrement(LookAhead);
+                        ParseTerm();
+                        AddToPostFix(Temp, NONE);
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+
+        private void ParseFactor()
+        {
+            string Temp;
+            switch(LookAhead)
+            {
+                case "(":
+                    MatchAndIncrement("(");
+                    ParseExpression();
+                    MatchAndIncrement(")");
+                    break;
+                case "NUM":
+                case "ID":
+                    AddToPostFix(LookAhead, LexicalAttribute);
+                    MatchAndIncrement(LookAhead);
+                    break;
+                case "FUNC":
+                    Temp = LexicalAttribute2;
+                    MatchAndIncrement("FUNC");
+                    MatchAndIncrement("(");
+                    ParseListOfExpressions();
+                    MatchAndIncrement(")");
+                    AddToPostFix(Temp, NONE);
+                    break;
+                default:
+                    SyntaxError();
+                    break;
+            }
+        }
+
+        private void ParseListOfExpressions()
+        {
+            ParseExpression();
+            while(true)
+            {
+                switch(LookAhead)
+                {
+                    case ",":
+                        MatchAndIncrement(LookAhead);
+                        ParseExpression();
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+
+        private void ParseTerm()
+        {
+            string Temp;
+            ParseFactor();
+            while(true)
+            {
+                switch(LookAhead)
+                {
+                    case "*":
+                    case "\\":
+                    case "/":
+                    case "MOD":
+                        Temp = LookAhead;
+                        MatchAndIncrement(LookAhead);
+                        ParseFactor();
+                        AddToPostFix(Temp, NONE);
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+
+        private void AddToPostFix(string TokenVal, int TokenAttr)
+        {
+            switch(TokenVal)
+            {
+                case "+":
+                case "-":
+                case "/":
+                case "\\":
+                case "*":
+                case "MOD":
+                    PostFixString = PostFixString + TokenVal;
+                    break;
+                case "NUM":
+                case "ID":
+                    PostFixString = PostFixString + TokenAttr.ToString() + " ";
+                    break;
+                default:
+                    PostFixString = PostFixString + TokenVal + " ";
+                    break;
+            }
+        }
+
+        private void SyntaxError()
+        {
+            if (Given == 0)
+            {
+                MessageBox.Show("Syntax Error", "Error");
+                Given = -1;
+            }
+            //Error 1
+        }
+
+        private bool IsAlpha(string TestNo)
+        {
+            if (TestNo.Length == 0)
+                return false;
+            char cTestNo = TestNo[0];
+            if((cTestNo >= 65 && cTestNo <= 90) || (cTestNo >= 97 && cTestNo <= 122))
+                return true;
+            else 
+                return false;
+        }
+
+        private bool IsAlphaNumberic(string TestNo)
+        {
+            if (TestNo.Length == 0)
+                return false;
+            char cTestNo = TestNo[0];
+            if ((cTestNo >= 48 && cTestNo <= 57) || (cTestNo >= 65 && cTestNo <= 90)
+                || (cTestNo >= 97 && cTestNo <= 122))
+                return true;
+            else
+                return false;
+        }
+
+        private bool IsNumber(string TestNo)
+        {
+            if (TestNo.Length == 0)
+                return false;
+            char cTestNo = TestNo[0];
+            if (cTestNo >= 48 && cTestNo <= 57)
+                return true;
+            else
+                return false;
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            SetVariables();
+            ParseAll();
+            Console.Write(PostFixString);
+            txtResult.Text = PostFixString;
+        }
+    }
+}
